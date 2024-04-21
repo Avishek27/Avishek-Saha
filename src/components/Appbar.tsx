d4a9415406bf35978728f87fc5d47dedbc34cbda
Avishek27 <sahaavi60@gmail.com>
2024-04-22 00:55:46 +0530

    initital

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -14,7 +14,6 @@ export default function Appbar(){
           className=
             "text-5xl drop-shadow-2xl pt-1 hover:scale-105 transition-all font-dancing-script text-white">
             
-<<<<<<< HEAD
             Avishek Saha
           </div>
           <div className="nav-links flex gap-x-8 text-xs md:text-base">
@@ -40,16 +39,5 @@ export default function Appbar(){
       </nav>
     </>
 
-=======
-            <h2 className = "font-dancing-script text-3xl p-3 mt-2 ml-7 ">Avishek Saha</h2>
-         </div>
-         <div className="text-white p-3 flex justify-between md:flex-row font-montserrat font-bold text-lg mr-14">
-         <div className="p-3 hover:text-green-500">About</div>
-            <div className="p-3 hover:text-green-500 cursor-pointer">Projects</div>
-            <div className="p-3 hover:text-green-500 cursor-pointer">Skills</div>
-            <div className="p-3 hover:text-green-500 cursor-pointer">Contact</div>
-            <div className="p-3 hover:text-green-500"><Socials/></div>
-            </div>
-    </div>
->>>>>>> 6e96dce6e5354034a551e1d8733f592649649e49
+
 }
