cf465abf0a004df009048418a3513189b391a6bc
Avishek27 <154351986+Avishek27@users.noreply.github.com>
2024-04-27 01:41:48 +0530

    Update Preabout.tsx

--- src/components/Preabout.tsx
+++ src/components/Preabout.tsx
@@ -3,30 +3,6 @@ import Avatar from './images/Avatar.png';
 import MotionDiv from './motion-div';
 // bg-[#36454F]
 export default function Preabout(){
-//     return <div className="bg-zinc-900 text-white ">
-//         <div className="flex flex-col items-center ">
-//             <div className="text-3xl font-bold font-montserrat p-5 pt-15 mt-10">
-//             Hi, I'm Avishek Saha! 👋
-//             </div>
-//             <div className="p-3 mb-5">
-//                 <img src = {Avatar}></img>
-//             </div>
-//             <div className = "text-2xl font-bold font-montserrat">
-//                 Software Engineer
-//             </div>
-//             <div className='text-xl font-montserrat font-medium'>
-//                 Full Stack Developer
-//             </div>
-//             <div className='text-xl font-montserrat font-medium mt-7'>
-//                 Welcome to my Portfolio!
-//                 </div>
-//                 <div className='text-xl font-montserrat font-medium'>
-//                 Typescript & C++ guy in the morning.Systems & networks kid at night.
-//                 </div>
-                
-//             </div>
-//     </div>
-// }
 
 return  (<section className="flex flex-col items-center justify-center bg-zinc-900 text-white py-8">
 <h1 className="mb-4 text-center text-lg md:text-[2rem]">
